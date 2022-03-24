28821763569769e0c6b9d4cb4dd728e8e2210a72
unknown <samiaabdelhaleem@gmail.com>
2022-03-24 11:10:32 +0200

    this is samia

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>HELLO WORLD</h1>
+  <h2>counter: {{ count }}</h2>
+
+  <div>
+    <button @click="store.commit('increment')">increment</button>
+    <button @click="store.commit('decrement')">decrement</button>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+const store = useStore();
+
+const count = computed(() => {
+  return store.state.counter;
+});
+</script>
